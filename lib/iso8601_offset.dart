0f14bdbf7b1d9b4cd7048130d1572940cbb544c9
Koji Wakamiya <koji.wakamiya@gmail.com>
2022-03-20 01:36:25 +0900

    Add converter for DateTime

--- lib/iso8601_offset.dart
+++ lib/iso8601_offset.dart
@@ -1,3 +1,4 @@
 library iso8601_offset;
 
+export 'src/datetime_converter.dart';
 export 'src/datetime_ext.dart';
---
+++ lib/src/datetime_converter.dart
@@ -0,0 +1,21 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:iso8601_offset/src/datetime_ext.dart';
+
+/// JsonConverter for non-null [DateTime] with localed offset
+class DateTimeConverter implements JsonConverter<DateTime, String> {
+  @override
+  DateTime fromJson(String json) => DateTime.parse(json).toLocal();
+
+  @override
+  String toJson(DateTime object) => object.toIso8601OffsetString();
+}
+
+/// JsonConverter for nullable [DateTime] with localed offset
+class TimeOrNullConverter implements JsonConverter<DateTime?, String?> {
+  @override
+  DateTime? fromJson(String? json) =>
+      json != null ? DateTime.parse(json).toLocal() : null;
+
+  @override
+  String? toJson(DateTime? object) => object?.toIso8601OffsetString();
+}
